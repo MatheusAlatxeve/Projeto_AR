317f82a516628a747c6a5cf21efc555961c17943
unknown <matheusalatxeve@gmail.com>
2025-09-15 00:38:39 -0300

    Configuracao inicial

---
+++ Assets/ARTouch.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARTouch : MonoBehaviour
+{
+
+
+    public GameObject UITest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary button.");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log("hit");
+                Debug.Log(hit.transform.name + " : " + hit.transform.tag);
+
+                if (hit.transform.tag == "carro_verde")
+                {
+                    Debug.Log("Carriot Verdidto");
+                    Vector3 pos = hit.point;
+                    pos.z += 0.25f;
+                    pos.y += 0.25f;
+                    Instantiate(UITest, pos, transform.rotation);
+                }
+
+                if (hit.transform.tag == "Info")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+
+            }
+        }
+    }
+}
