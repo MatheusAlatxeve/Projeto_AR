466306eea1b9e862417ce77e6270cd19f33cde0a
unknown <matheusalatxeve@gmail.com>
2025-09-29 03:58:19 -0300

    Atualizacao projeto

--- Assets/ARTouch.cs
+++ Assets/ARTouch.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
 public class ARTouch : MonoBehaviour
 {
+    public GameObject carroVerde;
+    public GameObject carroVermelho;
+    public GameObject carroAzul;
+    public GameObject carroAmarelo;
 
+    // audios
+    public AudioClip CarsclipVerde;
 
-    public GameObject UITest;
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip CarsclipNotVerde;
+
+    AudioSource audio;
+        void Start()
     {
-        
+        audio = GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
@@ -29,14 +37,64 @@ public class ARTouch : MonoBehaviour
 
                 if (hit.transform.tag == "carro_verde")
                 {
-                    Debug.Log("Carriot Verdidto");
+                    Debug.Log("Carro Verde");
                     Vector3 pos = hit.point;
                     pos.z += 0.25f;
                     pos.y += 0.25f;
-                    Instantiate(UITest, pos, transform.rotation);
+                    Instantiate(carroVerde, pos, transform.rotation);
+                    audio.PlayOneShot(CarsclipVerde, 1);
+
                 }
 
-                if (hit.transform.tag == "Info")
+                if (hit.transform.tag == "carro_vermelho")
+                {
+                    Debug.Log("Carro Vermelho");
+                    Vector3 pos = hit.point;
+                    pos.z += 0.25f;
+                    pos.y += 0.25f;
+                    Instantiate(carroVermelho, pos, transform.rotation);
+                    audio.PlayOneShot(CarsclipNotVerde, 1);
+
+                }
+
+                if (hit.transform.tag == "carro_azul")
+                {
+                    Debug.Log("Carro Azul");
+                    Vector3 pos = hit.point;
+                    pos.z += 0.15f;
+                    pos.y += 0.25f;
+                    Instantiate(carroAzul, pos, transform.rotation);
+                    audio.PlayOneShot(CarsclipNotVerde, 1);
+
+                }
+
+                if (hit.transform.tag == "carro_amarelo")
+                {
+                    Debug.Log("Carro Amarelo");
+                    Vector3 pos = hit.point;
+                    pos.z += 0.15f;
+                    pos.y += 0.25f;
+                    Instantiate(carroAmarelo, pos, transform.rotation);
+                    audio.PlayOneShot(CarsclipNotVerde, 1);
+
+                }
+
+                if (hit.transform.tag == "VerdeInfo")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+
+                if (hit.transform.tag == "VermelhoInfo")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+
+                if (hit.transform.tag == "AzulInfo")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+                
+                 if (hit.transform.tag == "AmareloInfo")
                 {
                     Destroy(hit.transform.gameObject);
                 }
